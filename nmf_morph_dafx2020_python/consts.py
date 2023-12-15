44a2d2155e80f70d8b51b536124271c9291f79fe
mic-tae <m.taenzer@gmx.net>
2023-12-15 10:19:05 +0100

    consts.py: small changes

--- nmf_morph_dafx2020_python/consts.py
+++ nmf_morph_dafx2020_python/consts.py
@@ -1,26 +1,34 @@
-from untwist.untwist.transforms import STFT, ISTFT
+from .untwist.untwist.transforms import STFT, ISTFT
+from .untwist.untwist.hpss import MedianFilterHPSS
 
 
 use_librosa = False
 
+# FFT and morph parameters
 fft_size = 2048
 hop_size = 512
 p = 0.9
-
-stft = STFT(fft_size=fft_size, hop_size=hop_size)
-istft = ISTFT(fft_size=fft_size, hop_size=hop_size)
-
 sr = None
 morph_meat = None
 
+# HPSS parameters
+hLength = 17
+pLength = 31
 
-#src = "../audio/01_cut.wav"
-#tgt = "../audio/02_cut.wav"
-#outfile = "../audio/0102cut"
+# Instantiate objects
+stft = STFT(fft_size=fft_size, hop_size=hop_size)
+istft = ISTFT(fft_size=fft_size, hop_size=hop_size)
+hpss = MedianFilterHPSS(hLength, pLength)
 
-src = "../audio/0_120_cut.wav"
-tgt = "../audio/1_125_cut.wav"
-outfile = "../audio/12070_v2_cut"
+
+## Files to use
+src = "../audio/01_cut.wav"
+tgt = "../audio/02_cut.wav"
+outfile = "../audio/0102cut"
+
+#src = "../audio/0_120_cut.wav"
+#tgt = "../audio/1_125_cut.wav"
+#outfile = "../audio/120125cut"
 
 #src = "../audio/00piano_cut.wav"
 #tgt = "../audio/01trumpet_cut_resampled.wav"
